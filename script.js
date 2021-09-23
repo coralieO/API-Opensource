c7e676a8b76b2f1901a120e68a12143e0bc2cd69
SGJipe <jeanpierre.han.206@gmail.com>
2021-09-23 23:40:29 +0200

    Almost complete javascript

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+const API_key = "75056b99a5ba4bfa18ac24ffdc0ad9f5";
+const url_genres = "https://api.themoviedb.org/3/genre/movie/list?api_key="+API_key+"&language=en-US";
+let html = "",
+    html_2 ="";
+
+fetch(url_genres)
+    .then(resp => resp.json())
+    .then(function(data) {
+        //console.log(data);
+        //console.log(typeof(data));
+        for(let i = 0, genresLength = data.genres.length; i < genresLength; i++) {
+            //console.log(data.length)
+            html = '<option value="'+data.genres[i].id+'">'+ data.genres[i].name+'</option>';
+            genres.innerHTML += html;
+        }
+    });
+
+submit.onclick = (e) => {
+    e.preventDefault;
+    const url_mvoviesByGenres = "https://api.themoviedb.org/3/discover/movie?api_key="+API_key+"&with_genres="+genres.value+"";
+    console.log(url_mvoviesByGenres);
+    fetch(url_mvoviesByGenres)
+        .then(response => response.json())
+        .then(function(data){
+            console.log(data);
+            console.log(typeof(data));
+            for(let j = 0, moviesByGenresLength = data.results.length; j < moviesByGenresLength; j++) {
+                html_2 = '<p>'+data.results[j].title+'<p>';
+                moviesByGenres.innerHTML += html_2;
+            }
+        })
+}
+
+
+/*
 const url = 'https://ghibliapi.herokuapp.com/films/';
 let html ="";
 //create an array
@@ -29,13 +64,38 @@ fetch(url)
             for (p = 0, asProdLength = asProducer.length; p < asProdLength; p++) {
                 //console.log(asProdLength);
                 //console.log(html);
-                html = '<li class="l-producers" id="'+p+'"> <a href="#"> ' + asProducer[p] + '</a> </li>';
+                html = '<li class="l-producers" id="producer'+p+'" value="'+p+'"> <a href="#"> ' + asProducer[p] + '</a> </li>';
                 producers = document.getElementById("producers");
                 producers.innerHTML += html;
             }
         }());
         let list_producers = document.getElementsByClassName('l-producers');
-        list_producers.onclick = CheckProducer(2);
+        console.log(list_producers);
+        producer0.onclick = () => {
+            let producerID = producer0.getAttribute("value");
+            movies.push(data.filter(datas => datas.producer == asProducer[producerID]));
+            console.log(movies);
+            html = '<table>';
+            html += '<tr>';
+            html += '<th> Title </th>';
+            html += '<th> Description </th>';
+            html += '</tr> <tr>';
+            for(i = 0, numberOfMovies = movies.length; i < numberOfMovies; i++) {
+                html += '<td>' + movies[i].title + ' </td>';
+                html += '<td>' + movies[i].description + ' </td>';
+            }
+            html += '</tr>';
+
+            let movies_HTML = document.getElementById("t-movies");
+            movies_HTML.innerHTML = html;
+        };
+        producer1.onclick = () => {
+            console.log(producer1.getAttribute("value"));
+        };
+        producer2.onclick = () => {
+            console.log(producer2.getAttribute("value"));
+        };
+        /*CheckProducer(2);
         console.log(list_producers.onclick);
         function CheckProducer(producerID) {
             if (producerID) {
@@ -45,3 +105,4 @@ fetch(url)
             }
         }
     })
+*/
